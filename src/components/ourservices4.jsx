2ba316a14907324d27f1a7cce363f98b1ce93911
Gustaxz <guxtavogusteible@gmail.com>
2025-01-20 11:44:14 -0300

    fix: header & navigation styles

--- src/components/ourservices4.jsx
+++ src/components/ourservices4.jsx
@@ -1,42 +1,42 @@
-import React from "react";
+import React from "react"
 
 export const OurServices4 = (props) => {
-  return (
-    <div id="about">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6 order-md-last">
-            <img src="img/laudo.jpg" className="img-fluid float-end" alt="" />
-          </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-            <h2>Projetos e Laudos</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+	return (
+		<div id="about" className="service4">
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-12 col-md-6 order-md-last">
+						<img src="img/laudo.jpg" className="img-fluid float-end" alt="" />
+					</div>
+					<div className="col-xs-12 col-md-6">
+						<div className="about-text">
+							<h2>Projetos e Laudos</h2>
+							<p>{props.data ? props.data.paragraph : "loading..."}</p>
 
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+							<div className="list-style">
+								<div className="col-lg-6 col-sm-6 col-xs-12">
+									<ul>
+										{props.data
+											? props.data.Why.map((d, i) => (
+													<li key={`${d}-${i}`}>{d}</li>
+											  ))
+											: "loading"}
+									</ul>
+								</div>
+								<div className="col-lg-6 col-sm-6 col-xs-12">
+									<ul>
+										{props.data
+											? props.data.Why2.map((d, i) => (
+													<li key={`${d}-${i}`}> {d}</li>
+											  ))
+											: "loading"}
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
