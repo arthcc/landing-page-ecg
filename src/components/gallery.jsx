2865bf522122eebdf72dc0ff6900b21acedaa6a0
Gustaxz <guxtavogusteible@gmail.com>
2025-02-05 17:50:35 -0300

    feat: gallery grid

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,35 +1,38 @@
-import { Image } from "./image";
 import React from "react";
+import { Image } from "./image";
 
 export const Gallery = (props) => {
-  return (
-    <div id="portfolio" className="text-center">
-      <div className="container">
-        <div className="section-title">
-          <h2>Galeria</h2>
-          <p>
-           Algumas de nossas obras.
-          </p>
-        </div>
-        <div className="row">
-          <div className="portfolio-items">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                    <Image
-                      title={d.title}
-                      largeImage={d.largeImage}
-                      smallImage={d.smallImage}
-                    />
-                  </div>
-                ))
-              : "Loading..."}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="portfolio" className="text-center">
+			<div className="container">
+				<div className="section-title">
+					<h2>Galeria</h2>
+					<p>Algumas de nossas obras.</p>
+				</div>
+				<div className="row">
+					<div
+						style={{
+							display: "grid",
+							gridTemplateColumns: "repeat(3, 1fr)",
+							gap: "1rem",
+						}}
+					>
+						{props.data
+							? props.data.map((d, i) => (
+									<div
+										key={`${d.title}-${i}`}
+										className="col-sm-6 col-md-4 col-lg-4"
+										style={{
+											width: "100%",
+										}}
+									>
+										<Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+									</div>
+							  ))
+							: "Loading..."}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
