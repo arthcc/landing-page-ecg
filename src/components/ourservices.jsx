10b6915b0e153d1e1cfa42f6f58339a62248c369
Gustaxz <guxtavogusteible@gmail.com>
2025-02-06 12:03:38 -0300

    feat: some changes

--- src/components/ourservices.jsx
+++ src/components/ourservices.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 
 export const OurServices = (props) => {
 	return (
@@ -7,7 +7,7 @@ export const OurServices = (props) => {
 				<div className="row">
 					<div className="col-xs-12 col-md-6">
 						{" "}
-						<img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+						<img src="img/construction.jpg" className="img-responsive" alt="" />{" "}
 					</div>
 					<div className="col-xs-12 col-md-6">
 						<div className="about-text">
@@ -18,18 +18,14 @@ export const OurServices = (props) => {
 								<div className="col-lg-6 col-sm-6 col-xs-12">
 									<ul>
 										{props.data
-											? props.data.Why.map((d, i) => (
-													<li key={`${d}-${i}`}>{d}</li>
-											  ))
+											? props.data.Why.map((d, i) => <li key={`${d}-${i}`}>{d}</li>)
 											: "loading"}
 									</ul>
 								</div>
 								<div className="col-lg-6 col-sm-6 col-xs-12">
 									<ul>
 										{props.data
-											? props.data.Why2.map((d, i) => (
-													<li key={`${d}-${i}`}> {d}</li>
-											  ))
+											? props.data.Why2.map((d, i) => <li key={`${d}-${i}`}> {d}</li>)
 											: "loading"}
 									</ul>
 								</div>
@@ -39,5 +35,5 @@ export const OurServices = (props) => {
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
